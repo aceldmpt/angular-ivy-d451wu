bfe326b00a5f84331e62d7130b9e9b1cfcac33bc
aceldmpt <aceldumpit@gmail.com>
2021-11-15 00:45:55 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-d451wu
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-d451wu)
---
+++ src/app/dumpit/dumpit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dumpit',
+  templateUrl: './dumpit.component.html',
+  styleUrls: ['./dumpit.component.css']
+})
+export class DumpitComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
